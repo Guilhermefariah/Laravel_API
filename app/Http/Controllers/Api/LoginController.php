9f7b6ddc028d27f0c5edd94f7327aefa9d7890d8
Guilhermefariah <Guilhermefaria321@gmail.com>
2025-02-08 20:57:11 -0300

    criado token

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -3,9 +3,32 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 
 class LoginController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        if (Auth::attempt(["email" => $request->email, "password" => $request->password])) {
+
+            $user = Auth::user();
+
+            $token = $request->user()->createToken("api-token")->plainTextToken;
+
+            return response()->json([
+                "status" => true,
+                "token" => $token,
+                "user" => $user
+            ], 200);
+
+        } else {
+            return response()->json([
+                "status" => false,
+                "message" => "Usuário ou senha inválidos"
+            ], 401);
+        }
+    }
 }
